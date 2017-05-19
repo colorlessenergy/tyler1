171935c480750459790c630bb82efd9603c837dc
colorlessenergy <brianmunozdev@gmail.com>
2017-05-19 13:30:02 -0700

    finished nav

---
+++ main.js
@@ -0,0 +1,6 @@
+var responsiveNav = document.getElementById("responsiveNav");
+var button = document.getElementById("button");
+
+button.addEventListener("click", function () {
+  responsiveNav.classList.toggle("toggle-nav")
+});
